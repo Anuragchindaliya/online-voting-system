5814ca5fa0bad73a4540fd7c58e33bc32cfab448
AnuragChindaliya <ak1148383@gmail.com>
2021-07-19 14:58:53 +0530

    refactor: content

--- admin/includes/candidates_modal.php
+++ admin/includes/candidates_modal.php
@@ -91,7 +91,7 @@
             <div class="modal-header">
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span></button>
-              <h4 class="modal-title"><b>Edit Voter</b></h4>
+              <h4 class="modal-title"><b>Edit Candidate</b></h4>
             </div>
             <div class="modal-body">
               <form class="form-horizontal" method="POST" action="candidates_edit.php">
--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
       <div class="pull-right hidden-xs">
         <b>All rights reserved</b>
       </div>
-      <strong>Copyright &copy; 2018-<?= date('Y');?> <a href="https://anurag-cv.web.app">Anurag</a></strong>
+      <strong>Copyright &copy; 2018-<?= date('Y');?> <a href="https://anurag-cv.web.app">Rajat</a></strong>
     </div>
     <!-- /.container -->
 </footer>
