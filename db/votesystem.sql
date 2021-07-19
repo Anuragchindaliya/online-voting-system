47d180cec4b90068ba66cda9c169ffbbc78552ad
AnuragChindaliya <ak1148383@gmail.com>
2021-07-19 23:07:18 +0530

    Database refactor

--- db/votesystem.sql
+++ db/votesystem.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 18, 2018 at 07:34 AM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.1.15
+-- Generation Time: Jul 19, 2021 at 07:31 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -44,7 +43,9 @@ CREATE TABLE `admin` (
 
 INSERT INTO `admin` (`id`, `username`, `password`, `firstname`, `lastname`, `photo`, `created_on`) VALUES
 (1, 'nurhodelta', '$2y$10$fLK8s7ZDnM.1lE7XMP.J6OuPbQ.DPUVKBo7rENnQY7gYq0xAzsKJy', 'Neovic', 'Devierte', 'facebook-profile-image.jpeg', '2018-04-02'),
-(2, 'anurag', '$2y$10$eK3OY9pe3qR90F5yQ9xr/uK52nArrGYC9LT/eQk8bS8UBwf0c9vNu', 'anurag', 'chindaliya', 'in-team-9.png', '2021-04-02');
+(2, 'anurag', '$2y$10$eK3OY9pe3qR90F5yQ9xr/uK52nArrGYC9LT/eQk8bS8UBwf0c9vNu', 'anurag', 'chindaliya', 'in-team-9.png', '2021-04-02'),
+(3, 'vasundhra', 'vasundhra', 'vasundhra', 'Mishra', '', '0000-00-00'),
+(6, 'rajat', '$2y$10$/2MUuL3W80TRLs6McuT/U.sTfUEbR/iNuf9JaE7EMUL3ZmvW/WY9C', 'Rajat', 'Kumar', 'team_bobby-370x290.jpg', '2021-04-02');
 
 -- --------------------------------------------------------
 
@@ -61,13 +62,12 @@ CREATE TABLE `candidates` (
   `platform` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
 --
 -- Dumping data for table `candidates`
 --
 
 INSERT INTO `candidates` (`id`, `position_id`, `firstname`, `lastname`, `photo`, `platform`) VALUES
-(18, 8, 'anurag', 'chindaliya', 'in-team-2.png', 'college election'),
+(18, 8, 'Rajat', 'Kumar', 'in-team-2.png', 'college election'),
 (19, 9, 'Narender', 'Modi', 'in-team-8.png', 'president of India not becoming the vice president of our college');
 
 -- --------------------------------------------------------
@@ -82,6 +82,10 @@ CREATE TABLE `positions` (
   `max_vote` int(11) NOT NULL,
   `priority` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `positions`
+--
 
 INSERT INTO `positions` (`id`, `description`, `max_vote`, `priority`) VALUES
 (8, 'President', 200, 1),
@@ -102,10 +106,17 @@ CREATE TABLE `voters` (
   `photo` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `voters`
+--
 
 INSERT INTO `voters` (`id`, `voters_id`, `password`, `firstname`, `lastname`, `photo`) VALUES
-(2, 'TImaeRjpFbV6SMU', '$2y$10$3qqGz6jHjNGt7rDwyOvff.AThcqNhaEyCnDvOml3wWtZ/xZjGK6iu', 'ankush', 'chindaliya', 'in-team-6.png'),
-(3, 'Ie8an7zoufgWpXR', '$2y$10$hsITNcUA23/Hj96kD/9hJOZR7DwirG7eUoqx7SDrFX8bCNZeoBCWm', 'kamal', 'gautam', 'in-testimoni-1.png');
+(2, 'TImaeRjpFbV6SMU', '$2y$10$PrSx80eQYxR7r6GtgnhHm.9v65VPMdF0Szm0pEO7CPjjnYfDQW32i', 'ankush', 'chindaliya', 'in-team-6.png'),
+(3, 'Ie8an7zoufgWpXR', '$2y$10$qjMhUh99XN3/731oAilrGe7wQQjeYotK3yYiv9H3f6D4VKNpJfQEq', 'kamal', 'gautam', 'in-testimoni-1.png'),
+(4, '84yMBpwCP16Kk5l', '$2y$10$II9Q8a9vpdHwgYSxrg.gsuadH1FqR7C5ruViPcugWON5FAzraEmJm', 'karan', 'gautam', 'in-team-3.png'),
+(5, 'cQXnOUrRzGolbMp', '$2y$10$3gqyoYWVs2aRfZ2NhRdsWOGjDHywwlxzl2t7T5QVdcqCePBgHYSKm', 'vasu', 'mishra', 'in-team-3.png'),
+(7, 'c9yWgz4Y6eq5tnk', '$2y$10$MplAIJ.BPf4vKOcidBAjaexgh8DjotD7yhFWlvRY1aOD4o5851g26', 'Rajat', 'kumar', 'man2-370x290.jpg'),
+(8, 'JX9ayt4bDNFG6VI', '$2y$10$ITHC6BzW5q59KaUj7r93RO.InZ0sZHVXDzznpGdV/ur5sN0I8Xnmm', 'akshay', 'kumar', 'dwayne-370x290.jpg');
 
 -- --------------------------------------------------------
 
@@ -120,9 +131,17 @@ CREATE TABLE `votes` (
   `position_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `votes`
+--
+
 INSERT INTO `votes` (`id`, `voters_id`, `candidate_id`, `position_id`) VALUES
 (81, 2, 18, 8),
-(82, 3, 18, 8);
+(82, 3, 18, 8),
+(83, 5, 18, 8),
+(84, 6, 18, 8),
+(85, 7, 19, 9),
+(86, 8, 19, 9);
 
 --
 -- Indexes for dumped tables
@@ -166,31 +185,31 @@ ALTER TABLE `votes`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `candidates`
 --
 ALTER TABLE `candidates`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `positions`
 --
 ALTER TABLE `positions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `voters`
 --
 ALTER TABLE `voters`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `votes`
 --
 ALTER TABLE `votes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
